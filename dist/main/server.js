da900718f7894db0fbf0a09b5cdcd7129acb5fa2
Rafael Dias <rafael.dias@10i9.com.br>
2021-01-13 14:34:08 -0300

    :heart: removendo dist e yarn.lock :heart:

--- dist/main/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-require("module-alias/register");
-const app_1 = __importDefault(require("@/main/config/app"));
-const env_1 = __importDefault(require("@/main/config/env"));
-app_1.default.listen(env_1.default.port, () => console.log(`Server running at: http://localhost:${env_1.default.port}`));
